baffc18a99fe6b99a33c19a02a139bcd5af9ba44
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-11-27 20:01:42 +0300

    Update commmon.mk

--- analysis_template/common.mk
+++ analysis_template/common.mk
@@ -1,12 +1,12 @@
-SCRIPT_DIR:=${HOME}/ccr_rates/ccr_scripts/
+SCRIPT_DIR:=${HOME}/ccr_rates/ccr_scripts
 
 ## MD traj parameters
-TRAJECTORY_PATH=${HOME}/olebedenko/bioinf/trj/ubq/tip4p-ew/NPT_bussi/bussi_box_8/5_run/
+TRAJECTORY_PATH=../../example_xtc_trj_ubq_NPT_bussi_box_8/
 REFERENCE_PDB_PATH:=${TRAJECTORY_PATH}/run00001.pdb
 TRAJECTORY_LENGTH:=9
 # type of MD run files "dat" - TrjtoolDatFile; "nc" - AmberNetCDF, "xtc" - GromacsXtcFile
-FILETYPE:=dat
+FILETYPE:=xtc
 # pattern of MD run files: run00001.dat ---> "run%05d".dat
 PATTERN:=run%05d
-#time per frame (ns)
+#time pe frame (ns)
 DT_NS=0.001
