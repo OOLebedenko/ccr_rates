040a73a8cc833a041a96d466b77c238d28991986
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-11-29 03:37:23 +0300

    Adjust scripts for upcoming pyxmolpp-2.0 release

--- ccr_scripts/extract_vectors.py
+++ ccr_scripts/extract_vectors.py
@@ -1,10 +1,13 @@
 from pyxmolpp2 import PdbFile, Trajectory, TrjtoolDatFile, GromacsXtcFile, AmberNetCDF
 from tqdm import tqdm
 import argparse
+import functools
+import operator
 import os
 from ccr_scripts.process_utils.select import get_CA_HA_selection, get_NH_selection, get_HA_HN_selection, \
     get_CO_selection, get_C_CA_selection, SelectionShifter
-from ccr_scripts.process_utils.extract import OpenCsvAsVectorsExtractors, Run
+from ccr_scripts.process_utils.extract import WriteVectorsToCsv, Run
+
 
 class XtcFileReaderWrapper:
     def __init__(self, n_frames):
@@ -26,7 +29,9 @@ if __name__ == '__main__':
     parser.add_argument('--output-directory', default=".")
     args = parser.parse_args()
 
-    trj_reader_dict = {"dat": TrjtoolDatFile, "nc": AmberNetCDF, "xtc": XtcFileReaderWrapper(args.frames_per_trajectory_file)}
+    trj_reader_dict = {"dat": TrjtoolDatFile,
+                       "nc": AmberNetCDF,
+                       "xtc": XtcFileReaderWrapper(args.frames_per_trajectory_file)}
 
     traj = Trajectory(PdbFile(args.path_to_reference_pdb).frames()[0])
     for ind in tqdm(range(1, args.trajectory_length + 1), desc="traj_reading"):
@@ -42,8 +47,12 @@ if __name__ == '__main__':
         "HA_HNp1": SelectionShifter(select_func=get_HA_HN_selection, shift_ind=1)
     }
 
-    for vector in args.vectors.split(","):
-        out_dir = os.path.join(args.output_directory, vector)
+    extractors = []
+    for vector_name in args.vectors.split(","):
+        out_dir = os.path.join(args.output_directory, vector_name)
         os.makedirs(out_dir, exist_ok=True)
-        with OpenCsvAsVectorsExtractors(traj[0], selections_dict[vector], out_dir=out_dir) as extract_vectors:
-            tqdm(traj, desc=vector) | extract_vectors | Run()
+        extractors.append(WriteVectorsToCsv(selections_dict[vector_name], output_directory=out_dir))
+
+    extract_vectors = functools.reduce(operator.or_, extractors)
+
+    tqdm(traj) | extract_vectors | Run()
