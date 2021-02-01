76a72a4122193bce2a363538027e581a6960cd8d
OOLebedenko <oolebedenko@gmail.com>
2021-02-01 15:53:47 +0300

    "add prefix in one and next residue for vectors in common.mk"

--- analysis_template/common.mk
+++ analysis_template/common.mk
@@ -1,7 +1,7 @@
-SCRIPT_DIR:=${HOME}/ccr_rates/ccr_scripts/ccr_scripts
+SCRIPT_DIR:=${HOME}/Projects/cross_relaxation_pyxmolpp2_v2/ccr_rates/ccr_scripts
 
 ## MD traj parameters
-TRAJECTORY_PATH=../../../tests_data/example_xtc_trj_ubq_NPT_bussi_box_8/
+TRAJECTORY_PATH=${HOME}/Projects/cross_relaxation_pyxmolpp2_v2/ccr_rates/tests/example_xtc_trj_ubq_NPT_bussi_box_8
 REFERENCE_PDB_PATH:=${TRAJECTORY_PATH}/run00001.pdb
 TRAJECTORY_LENGTH:=9
 # type of MD run files "dat" - TrjtoolDatFile; "nc" - AmberNetCDF, "xtc" - GromacsXtcFile
@@ -12,9 +12,10 @@ PATTERN:=run%05d
 DT_NS=0.001
 
 # specify all vectors that you need in all calculated CCRs
-VECTOR_1=N-H
-VECTOR_2=CA-HA|HA2|HA3
-VECTOR_3=C-O
-VECTOR_4=C-CA
-VECTOR_5=H-HA|HA2|HA3
-VECTORS=${VECTOR_1},${VECTOR_2},${VECTOR_3},${VECTOR_4},${VECTOR_5}
+VECTOR_1=N-H_one_residue
+VECTOR_2=CA-HA|HA2|HA3_one_residue
+VECTOR_3=C-O_one_residue
+VECTOR_4=C-CA_one_residue
+VECTOR_5=HA|HA2|HA3-H_one_residue
+VECTOR_6=HA|HA2|HA3-H_next_residue
+VECTORS=${VECTOR_1},${VECTOR_2},${VECTOR_3},${VECTOR_4},${VECTOR_5},${VECTOR_6}
