7b6b443bb659ccc891b8feef32cb4df4b54d556a
OOLebedenko <oolebedenko@gmail.com>
2021-03-10 16:35:53 +0300

    refractor selections of residues

--- ccr_scripts/process_utils/select.py
+++ ccr_scripts/process_utils/select.py
@@ -1,15 +1,5 @@
-from pyxmolpp2 import Residue, AtomSelection, Frame, aName
+from pyxmolpp2 import AtomSelection, Frame, aName
 from typing import Union, List
-
-
-def has_atoms(atom_names):
-    def predicate(residue: Residue):
-        if set(atom_names).intersection(set([atom.name for atom in residue.atoms])):
-            return True
-        else:
-            return False
-
-    return predicate
 
 
 def atom_pairs_from_consequent_residues(atom_names_1: Union[str, List[str]], atom_names_2: Union[str, List[str]]):
@@ -24,11 +14,10 @@ def atom_pairs_from_consequent_residues(atom_names_1: Union[str, List[str]], ato
         return AtomSelection()
 
     def selector(frame: Frame):
-        residues = frame.residues.filter(has_atoms(atom_names_1))
 
         return [
             (first_atom, second_atom)
-            for res in residues[:-1]
+            for res in frame.residues[:-1]
             for first_atom in res.atoms.filter(aName.is_in(*atom_names_1))
             for second_atom in to_atoms(res.next).filter(aName.is_in(*atom_names_2))
         ]
@@ -43,10 +32,10 @@ def atom_pairs_from_one_residue(atom_names_1: Union[str, List[str]], atom_names_
         atom_names_2 = [atom_names_2]
 
     def selector(frame: Frame):
-        residues = frame.residues.filter(has_atoms(atom_names_1))
+
         return [
             (first_atom, second_atom)
-            for r in residues
+            for r in frame.residues
             for first_atom in r.atoms.filter(aName.is_in(*atom_names_1))
             for second_atom in r.atoms.filter(aName.is_in(*atom_names_2))
         ]
